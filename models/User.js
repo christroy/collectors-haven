4c93854a182d229f359fde9a5f9cba122de93779
shaggy <simmons5family@gmail.com>
2021-02-26 01:01:59 -0600

    write User model, write regex validation for email & password

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema (
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId
+        },
+        email: {
+            type: String,
+            required: 'An email is Required!',
+            trim: true,
+            match: /^[a-zA-Z0-9.!?_-]+@[a-zA-Z-]+(?:\.[a-zA-Z0-9]+)$/,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: 'A Username is Required',
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: 'A Password is Required',
+            match: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+        }
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
