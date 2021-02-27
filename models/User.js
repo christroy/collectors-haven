df144a8679b12dfff8626c689825fe4fb4f5e13c
shaggy <simmons5family@gmail.com>
2021-02-26 23:22:04 -0600

    add password hashing with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const { Schema, model, Types } = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const UserSchema = new Schema (
     {
@@ -27,6 +29,34 @@ const UserSchema = new Schema (
         }
     }
 );
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified ( or is new )
+    if (!user.isModified('password')) return next();
+
+    // generate salt
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = model('User', UserSchema);
 
