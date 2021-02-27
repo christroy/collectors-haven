37408f28e5ba58f9627a03185713c804da062125
shaggy <simmons5family@gmail.com>
2021-02-26 20:44:39 -0600

    write Collection and Item Schemas. Items are embedded in collection, and collection refrences User

---
+++ models/Collection.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+    // collection references User, and has items embedded
+const CollectionSchema = new Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        items: [ItemSchema]
+    }
+);
+
+const ItemSchema = new Schema (
+    {
+        name: {
+            type: String,
+            required: 'Please enter a Name for the Item',
+            trim: true
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        condition: {
+            type: String,
+            trim: true
+        },
+        priceBought: {
+            type: Number
+        }
+    }
+)
+
+const Collection = model('Collection', CollectionSchema);
+
+module.exports = Collection; 
